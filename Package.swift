b814fd95b6fc5ebd2ce30496e269582dad1b6e19
Gustaf Rosenblad <gustaf@nightshifthabits.com>
2026-09-19 16:55:16 +0200

    chore: Bumped distributed-lock-swift to 0.1.0

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/valkey-io/valkey-swift", from: "1.0.0"),
-    .package(url: "https://github.com/rosecoder/distributed-lock-swift.git", from: "0.0.3"),
+    .package(url: "https://github.com/rosecoder/distributed-lock-swift.git", from: "0.1.0"),
   ],
   targets: [
     .target(
